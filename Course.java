b282c6187b81b7f00f9aea34308421ab3a0e0565
ProfWendi <jollymor@sheridancollege.ca>
2024-07-14 15:17:55 -0400

    copied from pastebin

---
+++ Course.java
@@ -0,0 +1,63 @@
+/**
+ * I didn't add javadocs: add some!!
+ * 
+ * @author Wendi
+ */
+public class Course implements Comparable<Course> {
+    
+    private String code;
+    private double grade;
+    
+    public Course(String code, double grade) {
+        setCode(code);
+        setGrade(grade);
+    }
+    
+    public String getCode() {
+        return code;
+    }
+    
+    public void setCode(String code) {
+        if (code != null && !code.trim().isEmpty() && code.matches("[a-zA-Z]{4}\\d{5}"))
+            this.code = code;
+        else
+            throw new IllegalArgumentException("Course code can't be empty.");
+    }
+    
+    public double getGrade() {
+        return grade;
+    }
+    
+    public void setGrade(double grade) {
+        if (grade >= 0)
+            this.grade = grade;
+        else
+            throw new IllegalArgumentException("Grade can't be a negative value.");
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%-9s: %6.2f", code, grade);
+    }
+    
+    // todo: hash
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (this == obj)
+            return true;
+        
+        if (obj instanceof Course) {
+            Course c = (Course)obj;
+            return c.getCode().equalsIgnoreCase(code);
+        } else
+            return false;
+    }
+
+    @Override
+    public int compareTo(Course c) {
+        return code.compareTo(c.getCode());
+    }   
+    
+}
