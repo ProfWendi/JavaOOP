d6b0ab1e1763e9fcac276040e8175d85931d0aca
ProfWendi <jollymor@sheridancollege.ca>
2024-07-17 16:31:30 -0400

    added title and credits

--- Course.java
+++ Course.java
@@ -6,10 +6,18 @@
 public class Course implements Comparable<Course> {
     
     private String code;
+    private String title;
+    private double credits;
     private double grade;
     
-    public Course(String code, double grade) {
+    public Course(String code, String title, double credits) {
         setCode(code);
+        setTitle(title);
+        setCredits(credits);        
+    }
+
+    public Course(String code, String title, double credits, double grade) {
+        this(code, title, credits);
         setGrade(grade);
     }
     
@@ -24,6 +32,25 @@ public class Course implements Comparable<Course> {
             throw new IllegalArgumentException("Course code can't be empty.");
     }
     
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setCredits(double credits) {
+        if (credits > 0)
+            this.credits = credits;
+        else
+            this.credits = 3.0;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
     public double getGrade() {
         return grade;
     }
@@ -35,6 +62,32 @@ public class Course implements Comparable<Course> {
             throw new IllegalArgumentException("Grade can't be a negative value.");
     }
     
+    public double gradePoints() {
+        if (grade >= 90)
+            return 4.0;
+        else if (grade >= 85)
+            return 3.8;
+        else if (grade >= 80)
+            return 3.6;
+        else if (grade >= 75) 
+            return 3.3;
+        else if (grade >= 70)
+            return 3.0;
+        else if (grade >= 65)
+            return 2.5;
+        else if (grade >= 60)
+            return 3.0;
+        else if (grade >= 55)
+            return 1.5;
+        else if (grade >= 50)
+            return 1.0;
+        else return 0;
+    }
+
+    public double weightedCreditValue() {
+        return gradePoints() * credits;
+    }
+
     @Override
     public String toString() {
         return String.format("%-9s: %6.2f", code, grade);
@@ -53,6 +106,7 @@ public class Course implements Comparable<Course> {
         } else
             return false;
     }
+	
 	@Override
 	public int hashCode() {
 		final int prime = 89;
@@ -64,6 +118,5 @@ public class Course implements Comparable<Course> {
     @Override
     public int compareTo(Course c) {
         return code.compareTo(c.getCode());
-    }   
-    
+    }       
 }
